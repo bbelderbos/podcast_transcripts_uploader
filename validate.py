7d445a809c1b997dc173eb62bdce07333be7fec5
Bob Belderbos <bobbelderbos@gmail.com>
2024-05-29 12:55:48 +0200

    validate script

---
+++ validate.py
@@ -0,0 +1,24 @@
+from pprint import pprint as pp
+
+import feedparser
+
+from script import FEED
+
+
+def all_episodes_have_transcripts(feed=FEED) -> bool:
+    entries = feedparser.parse(feed).entries
+    for entry in entries:
+        try:
+            if not entry.podcast_transcript["url"]:
+                return False
+        except (AttributeError, KeyError):
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    result = all_episodes_have_transcripts()
+    if result:
+        print("All episodes have transcripts.")
+    else:
+        print("Some episodes are missing transcripts.")
